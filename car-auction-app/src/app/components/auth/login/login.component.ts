67a0b36984b3b2f1a2524cf35e92a0594830d2d5
Samir Pomak <samir.pomak@tick42.com>
2024-03-02 17:27:19 +0200

    add login view

---
+++ car-auction-app/src/app/components/auth/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  loginInProgress = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private messageService: MessageService
+  ) {}
+  loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  async handleSubmit() {
+    if (this.loginForm.valid) {
+      this.loginInProgress = true;
+      this.userService
+        .login(this.loginForm.getRawValue())
+        .then((credentials) => {
+          console.log({ credentials });
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success!',
+            detail: 'You have been signed in successfully!',
+          });
+        })
+        .catch((error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.message,
+          });
+        })
+        .finally(() => {
+          this.loginInProgress = false;
+        });
+    }
+  }
+}
