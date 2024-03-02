8824fe03112b23cf9635bf5d1e473a69e0f5e8a5
Samir Pomak <samir.pomak@tick42.com>
2024-03-02 17:26:36 +0200

    add user service

---
+++ car-auction-app/src/app/services/user.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {
+  Auth,
+  user,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { shareReplay } from 'rxjs';
+
+type UserCredentials = {
+  email: string;
+  password: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private user = user(this.auth).pipe(shareReplay(1));
+  constructor(private auth: Auth) {}
+
+  getUserObservable() {
+    return this.user;
+  }
+
+  register(credentials: UserCredentials) {
+    return createUserWithEmailAndPassword(
+      this.auth,
+      credentials.email,
+      credentials.password
+    );
+  }
+
+  login(credentials: UserCredentials) {
+    return signInWithEmailAndPassword(
+      this.auth,
+      credentials.email,
+      credentials.password
+    );
+  }
+
+  signOut() {
+    return signOut(this.auth);
+  }
+}
