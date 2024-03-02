4297bb416ec743f5d2dba5e08a8b834f2a6515d4
Samir Pomak <samir.pomak@tick42.com>
2024-03-02 17:27:04 +0200

    add register view

---
+++ car-auction-app/src/app/components/auth/register/register.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { UserService } from 'src/app/services/user.service';
+import { CustomValidators } from 'src/app/utils/CustomValidators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  submitInProgress = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private messageService: MessageService
+  ) {}
+  registerForm = this.formBuilder.nonNullable.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    },
+    { validators: [CustomValidators.MatchingPasswords] }
+  );
+
+  async handleSubmit() {
+    if (this.registerForm.valid) {
+      this.submitInProgress = true;
+      this.userService
+        .register(this.registerForm.getRawValue())
+        .then((credentials) => {
+          console.log({ credentials });
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success!',
+            detail: 'You have been registered successfully!',
+          });
+        })
+        .catch((error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: error.message,
+          });
+        })
+        .finally(() => {
+          this.submitInProgress = false;
+        });
+    }
+  }
+}
