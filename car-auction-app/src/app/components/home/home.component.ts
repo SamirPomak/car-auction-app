aa812944bf51a2d7e695ed56e02a062fdf705029
Samir Pomak <samir.pomak@tick42.com>
2024-03-16 16:44:50 +0200

    add featured auctions sections on home

--- car-auction-app/src/app/components/home/home.component.ts
+++ car-auction-app/src/app/components/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Auction } from 'src/app/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  auctions: Auction[] = [];
+  loadingAuctions = true;
+  constructor(
+    private firestore: Firestore,
+    private destroyerRef: DestroyRef,
+    private router: Router
+  ) {
+    const auctionCollection = collection(this.firestore, 'auctions');
+    collectionData(auctionCollection)
+      .pipe(takeUntilDestroyed(this.destroyerRef))
+      .subscribe(async (data) => {
+        this.loadingAuctions = false;
+        this.auctions = data.slice(data.length - 4) as Auction[];
+        console.log(data);
+      });
+  }
+  onAuctionClick(id: string) {
+    this.router.navigate(['auctions/details', id]);
+  }
 
+  onAuctionsClick() {
+    this.router.navigate(['auctions']);
+  }
 }
