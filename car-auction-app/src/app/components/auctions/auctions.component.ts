e3149a90be5b3db390469bd147a937033982b035
Samir Pomak <samir.pomak@tick42.com>
2024-03-17 15:45:35 +0200

    enable sorting by price for auctions 💲

--- car-auction-app/src/app/components/auctions/auctions.component.ts
+++ car-auction-app/src/app/components/auctions/auctions.component.ts
@@ -1,7 +1,8 @@
 import { Component, DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest, debounceTime } from 'rxjs';
+import { SelectItem } from 'primeng/api';
+import { combineLatest, debounceTime, firstValueFrom } from 'rxjs';
 import { AuctionService } from 'src/app/services/auction.service';
 import { UserService } from 'src/app/services/user.service';
 import { Auction } from 'src/app/types';
@@ -15,6 +16,13 @@ export class AuctionsComponent {
   mode: 'myAuctions' | undefined;
   loadingAuctions = true;
   auctions: Auction[] = [];
+  sortOrder: number | undefined;
+  sortField: string | undefined;
+  sortOptions: SelectItem[] = [
+    { label: 'Unsorted', value: 'price' },
+    { label: 'Price High to Low', value: 'price_high' },
+    { label: 'Price Low to High', value: 'price_low' },
+  ];
   constructor(
     private destroyerRef: DestroyRef,
     private route: ActivatedRoute,
@@ -34,6 +42,7 @@ export class AuctionsComponent {
       .pipe(takeUntilDestroyed(this.destroyerRef))
       .subscribe(async ([query, data]) => {
         this.loadingAuctions = false;
+
         if (this.mode === 'myAuctions') {
           const userId = (await userService.getUser())?.uid;
           this.auctions = (data as Auction[]).filter(
@@ -47,7 +56,6 @@ export class AuctionsComponent {
               .includes(query)
           );
         }
-        console.log(data);
       });
   }
 
@@ -57,5 +65,26 @@ export class AuctionsComponent {
 
   onAuctionEditClick(id: string) {
     this.router.navigate(['auctions/edit', id]);
+  }
+
+  async onSortChange(event: any) {
+    let value = event.value;
+
+    switch (value) {
+      case 'price_high':
+        this.sortOrder = -1;
+        this.sortField = value.split('_')[0];
+        break;
+      case 'price_low':
+        this.sortOrder = 1;
+        this.sortField = value.split('_')[0];
+        break;
+      default:
+        this.sortOrder = undefined;
+        this.sortField = undefined;
+        this.auctions = (await firstValueFrom(
+          this.auctionService.getAllAuctionsObserver()
+        )) as Auction[];
+    }
   }
 }
